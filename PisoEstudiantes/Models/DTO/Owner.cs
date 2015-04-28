c2de5ae1e68f222ee8b77c9ed771db3e5839a42d
noelou93 <noelou.93@gmail.com>
2015-04-28 09:40:51 +0200

    Primer Sprint

---
+++ PisoEstudiantes/Models/DTO/Owner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PisoEstudiantes.Models.DTO
+{
+    public class Owner : User
+    {
+        //TODO
+        private List<Renter> lRenters;
+        private List<Flat> lFlats;
+
+        
+
+        public List<Flat> LFlats
+        {
+            get { return lFlats; }
+            set { lFlats = value; }
+        }
+
+        public List<Renter> LRenters
+        {
+            get { return lRenters; }
+            set { lRenters = value; }
+        }
+    }
+}
